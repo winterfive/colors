4fbdd719779aee931989443910bbdbd9a50fee0c
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-06-06 11:30:33 -0400

    Changed font-family to Roboto

--- src/App.vue
+++ src/App.vue
@@ -20,11 +20,11 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: blue;
   margin-top: 60px;
 }
 </style>
