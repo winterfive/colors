09b002a589f9d48eea5a230a18de01feed9400f9
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-06-06 11:30:56 -0400

    Added material design icons

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import Vuetify from 'vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+Vue.use(Vuetify, {
+  iconfont: 'md'
+})
