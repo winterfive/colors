ca280dc0c3da41dd843aedbab1bd7dbc590d490a
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-06-06 11:31:08 -0400

    Added placeholder content

--- src/components/Square.vue
+++ src/components/Square.vue
@@ -1,5 +1,13 @@
 <template>
-  <v-icon>looks_one</v-icon>
+  <v-container fluid>
+    <v-layout justify-space-around class="mb-2">
+      <span class="group pa-2">
+        <v-icon>home</v-icon>
+        <v-icon>event</v-icon>
+        <v-icon>info</v-icon>
+      </span>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
