23c6eb54f65ec3f863183245def212fb42cf90ad
Lee Gainer <piealacode@gmail.com>
2019-06-19 16:47:23 -0400

    Updating everything to handle vuetify

---
+++ src/components/Nav.vue
@@ -0,0 +1,31 @@
+
+
+<script>
+  export default {
+    data: () => ({
+      color: '#8E00FF',
+      hideCanvas: false,
+      hideInputs: false,
+      hideModeSwitch: false,
+      mode: 'rgba',
+      modes: ['rgba', 'hsla', 'hexa'],
+      showSwatches: false,
+    }),
+  }
+</script>
+
+<template>
+  <div>
+    <v-color-picker class="e4"></v-color-picker>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.e4 {
+width: 400px;
+margin: auto;
+}
+
+</style>
+
+
--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 import { store } from "./store";
+import vSelect from 'vue-select';
 
 Vue.config.productionTip = false
 
 Vue.use(Vuetify, {
   iconfont: 'md'
 })
+
+Vue.component('v-select', vSelect)
 
 new Vue({
   store,
