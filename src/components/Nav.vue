61f5f6af01693e77ce7327999120a5dfae3cecc6
Lee Gainer <piealacode@gmail.com>
2019-06-19 17:04:05 -0400

    Working on color slider

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,23 +1,38 @@
-
-
 <script>
   export default {
     data: () => ({
-      color: '#8E00FF',
-      hideCanvas: false,
-      hideInputs: false,
-      hideModeSwitch: false,
-      mode: 'rgba',
-      modes: ['rgba', 'hsla', 'hexa'],
-      showSwatches: false,
+      colors: [
+        { color:'Red', red: '255', green:'0', blue:'0'},
+        { color: 'Red-Orange', red:'254', green:'97', blue:'1'},
+        { color: 'Orange', value:'254,147,1'},
+
+      ]
     }),
+
+    methods: {
+      season (val) {
+        return this.icons[val]
+      },
+    },
   }
 </script>
 
 <template>
-  <div>
-    <v-color-picker class="e4"></v-color-picker>
-  </div>
+  <v-layout>
+    <v-flex pa-5>
+      <v-range-slider
+        :tick-labels="colors"
+        :value="[0, 1]"
+        min="0"
+        max="20"
+        ticks="always"
+        tick-size="4"
+      >
+        <template v-slot:thumb-label="props">
+        </template>
+      </v-range-slider>
+    </v-flex>
+  </v-layout>
 </template>
 
 <style lang="scss" scoped>
