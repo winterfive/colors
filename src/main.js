54145cc3de9196327ce5dd00e16ae32caf1998fc
Lee Gainer <piealacode@gmail.com>
2019-06-19 12:27:00 -0400

    Added a store

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vuetify from 'vuetify'
+import { store } from "./store";
 
 Vue.config.productionTip = false
 
@@ -10,6 +11,7 @@ Vue.use(Vuetify, {
 })
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
 
