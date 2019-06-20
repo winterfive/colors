8fbb85067de63f2da0fc77159467c115d7b5b3d1
Lee Gainer <piealacode@gmail.com>
2019-06-20 19:02:14 -0400

    Removed v-select code

--- src/main.js
+++ src/main.js
@@ -5,15 +5,12 @@ import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import { store } from "./store";
-import vSelect from 'vue-select';
 
 Vue.config.productionTip = false
 
 Vue.use(Vuetify, {
   iconfont: 'md'
 })
-
-Vue.component('v-select', vSelect)
 
 new Vue({
   store,
