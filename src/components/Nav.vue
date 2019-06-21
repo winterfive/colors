0fd5ca9352bdb10f3dde5a3d768772e3548c0b92
Lee Gainer <piealacode@gmail.com>
2019-06-21 10:42:48 -0400

    v-model value is an array so so changing handling of each value

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,28 +2,41 @@
   export default {
     computed: {
       color () {
-        if (this.tickValue === 1) return 'indigo'
-        if (this.tickValue === 2) return 'teal'
-        if (this.tickValue === 3) return 'green'
-        if (this.tickValue === 4) return 'orange'
-        return 'red'
+        this.value.forEach(v => {
+          if (v === 1) return 'indigo'
+          if (v === 2) return 'teal'
+          if (v === 3) return 'green'
+          if (v === 4) return 'orange'
+          return 'red';         
+        }        
       },
-      tickValue: 0
+    },
+    methods: {
+      consoleLog() {
+        // eslint-disable-next-line no-console
+        console.log("value: " + this.value);
+      }
+    },
+    data() {
+      return {
+        value: [2,3]
+      }
+    }
   }
-}
 </script>
 
 <template>
     <v-layout>
       <v-flex pa-5>
         <v-range-slider
-          v-model="tickValue"
+          v-model.lazy="value"
           min="0"
           max="20"
           ticks
           tick-size="10"
           :color="color"
           always dirty
+          @change="consoleLog"
         >
         </v-range-slider>
       </v-flex>
@@ -31,10 +44,6 @@
 </template>
 
 <style lang="scss" scoped>
-.e4 {
-width: 400px;
-margin: auto;
-}
 
 </style>
 
