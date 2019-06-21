737e2cd4501f0ee6c31edc9143c5b13aeaae6bc3
Lee Gainer <piealacode@gmail.com>
2019-06-21 18:09:21 -0400

    Added vue-color to app

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,60 +1,30 @@
 <script>
   export default {
-    computed: {
-      colors: function() {
-        this.tickValues.forEach(v => {
-        if(v === 0) return 'blue'
-        if(v === 1) return 'green'
-        if(v === 2) return 'yellow'
-        if(v === 3) return 'black'
-        if(v === 4) return 'purple'
-        
-      })
-      return 'red'
-      }      
-    },
   data() {
     return {
-      tickValues: [0,1],
-      colors: ['blue', 'green']
-      // Can colors be an array? Do I need to use 2 different sliders?
+      colors: 'red'
     }
   },
-  methods: {
-    setColor() {
-      // eslint-disable-next-line no-console
-      console.log("colors: " + this.colors[0] + this.colors[1]);
-      // eslint-disable-next-line no-console
-      console.log("tickValue is: " + this.tickValues)
-
-      this.tickValues.forEach(v => {
-        if(v === 0) return 'blue'
-        if(v === 1) this.colors[v.index] = 'green'
-        if(v === 2) this.colors[v.index] = 'yellow'
-        if(v === 3) this.colors[v.index] = 'black'
-        if(v === 4) this.colors[v.index] = 'purple'
-      })
-    }
-  }
+  methods: {}
 }
 </script>
 
 <template>
-    <v-layout>
-      <v-flex pa-5>
-        <v-range-slider
-          v-model="tickValues"
-          min="0"
-          max="20"
-          ticks
-          tick-size="10"
-          :color="blue"
-          always dirty
-          @change="setColor"
-        >
-        </v-range-slider>
-      </v-flex>
-    </v-layout>
+<v-container>
+  <v-layout>
+    <v-flex>
+      <compact-picker v-model="colors" />
+      <chrome-picker :value="colors" @input="updateValue"></chrome-picker>
+      <div>
+    <v-btn color="success">Success</v-btn>
+    <v-btn color="error">Error</v-btn>
+    <v-btn color="warning">Warning</v-btn>
+    <v-btn color="info">Info</v-btn>
+  </div>
+    </v-flex>
+  </v-layout>
+</v-container>
+  
 </template>
 
 <style lang="scss" scoped>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
-import './plugins/vuetify'
 import App from './App.vue'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-import { store } from "./store";
-
-Vue.config.productionTip = false
+import './plugins/vuetify'
+import { store } from "./store"
+import { Photoshop } from 'vue-color'
 
 Vue.use(Vuetify, {
   iconfont: 'md'
@@ -14,6 +13,7 @@ Vue.use(Vuetify, {
 
 new Vue({
   store,
+  'photoshop-picker': Photoshop,
   render: h => h(App),
 }).$mount('#app')
 
