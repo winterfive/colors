a7eca039db4c9bf806e3be321c3f77ef6b1f49ef
Lee Gainer <piealacode@gmail.com>
2019-06-21 12:50:02 -0400

    Working on changing each thumbs color as user makes changes

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,37 +1,56 @@
 <script>
   export default {
-    data() {
-      return {
-        value: [2,3],
-        color: ['blue', 'green']
-      }
-    }, 
-    methods: {
-      color() {
-        this.value.forEach(v => {
-          // switch statement        
-        }
-      },
-      consoleLog() {
-        // eslint-disable-next-line no-console
-        console.log("value: " + this.value);
-      }
+    computed: {
+      colors: function() {
+        this.tickValues.forEach(v => {
+        if(v === 0) return 'blue'
+        if(v === 1) return 'green'
+        if(v === 2) return 'yellow'
+        if(v === 3) return 'black'
+        if(v === 4) return 'purple'
+        
+      })
+      return 'red'
+      }      
+    },
+  data() {
+    return {
+      tickValues: [0,1],
+      colors: ['blue', 'green']
+      // Can colors be an array? Do I need to use 2 different sliders?
+    }
+  },
+  methods: {
+    setColor() {
+      // eslint-disable-next-line no-console
+      console.log("colors: " + this.colors[0] + this.colors[1]);
+      // eslint-disable-next-line no-console
+      console.log("tickValue is: " + this.tickValues)
+
+      this.tickValues.forEach(v => {
+        if(v === 0) return 'blue'
+        if(v === 1) this.colors[v.index] = 'green'
+        if(v === 2) this.colors[v.index] = 'yellow'
+        if(v === 3) this.colors[v.index] = 'black'
+        if(v === 4) this.colors[v.index] = 'purple'
+      })
     }
   }
+}
 </script>
 
 <template>
     <v-layout>
       <v-flex pa-5>
         <v-range-slider
-          v-model.lazy="value"
+          v-model="tickValues"
           min="0"
           max="20"
           ticks
           tick-size="10"
-          :color="color"
+          :color="blue"
           always dirty
-          @change="consoleLog"
+          @change="setColor"
         >
         </v-range-slider>
       </v-flex>
@@ -39,5 +58,4 @@
 </template>
 
 <style lang="scss" scoped>
-
 </style>
